3562a00edda2053f8f95ef53df78c986be542cdd
HelloPay Admin <hellopay@admin.com>
2026-04-09 08:58:38 +0530

    chore: migrate Firebase configuration to environment variables and update project ID

--- app/FirebaseManager.tsx
+++ app/FirebaseManager.tsx
@@ -6,13 +6,13 @@ import { useEffect } from "react";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAiPfeohL4ikZ1bZY7mk7G4dON6fSN_qu4",
-  authDomain: "hellopay-89da2.firebaseapp.com",
-  projectId: "hellopay-89da2",
-  storageBucket: "hellopay-89da2.firebasestorage.app",
-  messagingSenderId: "640617233547",
-  appId: "1:640617233547:web:0e245ad4b495b6722d688e",
-  measurementId: "G-MJN7ZW9TM2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export default function FirebaseManager() {
